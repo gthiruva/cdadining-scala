e2293b592b8bd59e883b81783a4f8de1126795ce
George Thiruvathukal <george@thiruva.com>
2012-11-10 02:37:41 -0800

    CDA Beta Version

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,7 +7,36 @@ object Application extends Controller {
   
   def index = Action {
 //    Ok(views.html.index("Your new application is ready."))
-      Ok(views.html.index())
+      Ok(views.html.index(""))
   }
-  
+  def about = Action {
+    Ok(views.html.about("Hi George's html.about!."))
+  }
+  def careers = Action {
+    Ok(views.html.careers("Hi George's html.reserve!."))
+  }
+  def events = Action {
+    Ok(views.html.events("Hi George's html.events!."))
+  }
+  def investors = Action {
+    Ok(views.html.investors("Hi George's html.reserve!."))
+  }
+  def menu = Action {
+    Ok(views.html.menu("Hi George's html.menu!."))
+  }
+  def mixology = Action {
+    Ok(views.html.mixology("Hi George's html.mini!."))
+  }
+  def order = Action {
+    Ok(views.html.order("Hi George's html.order!."))
+  }
+  def photos = Action {
+    Ok(views.html.photos("Hi George's html.photos!."))
+  }
+  def press = Action {
+    Ok(views.html.press("Hi George's html.press!."))
+  }
+  def reserve = Action {
+    Ok(views.html.reserve("Hi George's html.reserve!."))
+  }
 }
